171676ece098c38ce99118a04ce044e883454290
ErickNelser <nelserdalas@gmail.com>
2026-09-03 21:10:29 -0400

    init_0
    primer commit

---
+++ GRUP._LOBO2_OFICIAL/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GRUP._LOBO2_OFICIAL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
